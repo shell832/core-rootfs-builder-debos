03f5244849a57628eb006414078bfc3632d5cb89
Your Name <your_email@whatever.com>
2020-08-20 00:55:31 -0500

    .

--- scripts/add-mainline-repos.sh
+++ scripts/add-mainline-repos.sh
@@ -7,26 +7,26 @@ echo "nameserver 1.1.1.1" > /etc/resolv.conf
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
-echo "deb http://repo.ubports.com/ bionic_-_edge_-_mesa main" >> /etc/apt/sources.list.d/ubports.list
-echo "deb http://repo.ubports.com/ bionic_-_edge_-_pine main" >> /etc/apt/sources.list.d/ubports.list
-echo "deb http://repo.ubports.com/ bionic_-_edge_-_wayland main" >> /etc/apt/sources.list.d/ubports.list
+#echo "deb http://repo.ubports.com/ bionic_-_edge_-_mesa main" >> /etc/apt/sources.list.d/ubports.list
+echo "deb http://repo.ubports.com/ xenial_-_edge_-_pine main" >> /etc/apt/sources.list.d/ubports.list
+echo "deb http://repo.ubports.com/ bionic_-_xwayland main" >> /etc/apt/sources.list.d/ubports.list
+
+#echo "" >> /etc/apt/preferences.d/ubports.pref
+#echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
+#echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
+#echo "Pin: release o=UBports,a=bionic_-_edge_-_mesa" >> /etc/apt/preferences.d/ubports.pref
+#echo "Pin-Priority: 2001" >> /etc/apt/preferences.d/ubports.pref
 
 echo "" >> /etc/apt/preferences.d/ubports.pref
 echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
-echo "Pin: release o=UBports,a=bionic_-_edge_-_mesa" >> /etc/apt/preferences.d/ubports.pref
-echo "Pin-Priority: 2001" >> /etc/apt/preferences.d/ubports.pref
-
-echo "" >> /etc/apt/preferences.d/ubports.pref
-echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
-echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
-echo "Pin: release o=UBports,a=bionic_-_edge_-_pine" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: release o=UBports,a=xenial_-_edge_-_pine" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin-Priority: 2002" >> /etc/apt/preferences.d/ubports.pref
 
 echo "" >> /etc/apt/preferences.d/ubports.pref
 echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
-echo "Pin: release o=UBports,a=bionic_-_edge_-_wayland" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: release o=UBports,a=bionic_-xwayland" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin-Priority: 2002" >> /etc/apt/preferences.d/ubports.pref
 
 apt update
